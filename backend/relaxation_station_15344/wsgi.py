0ad42ad1ea1c62b7c1e3b47d682ab319854139e4
Crowdbotics <team@crowdbotics.com>
2020-04-02 19:57:53 +0000

    Initial commit.

---
+++ backend/relaxation_station_15344/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for relaxation_station_15344 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'relaxation_station_15344.settings')
+
+application = get_wsgi_application()
